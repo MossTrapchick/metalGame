32f477d1e5750d8c32ebc1e4663dc0abd587a70f
Serugei <serugei.alt@mail.ru>
2025-04-26 19:39:41 +0300

    Main menu

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioMixerGroup Mixer;
+    public static UnityEvent<AudioClip> Play = new();
+    public static UnityEvent<float> ChangePitch = new();
+    private AudioSource source;
+    [SerializeField] Slider soundSlider, musicSlider, masterSlider;
+    void play(AudioClip clip) => source.PlayOneShot(clip);
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        Play.AddListener(play);
+        ChangePitch.AddListener(SetPitch);
+    }
+    private void Start()
+    {
+        setCurentVolume(soundSlider);
+        setCurentVolume(musicSlider);
+        setCurentVolume(masterSlider);
+    }
+    private string mixerGroup;
+    public string MixerGroup { set { mixerGroup = value; } }
+    void setCurentVolume(Slider slider)
+    {
+        float t;
+        if (PlayerPrefs.HasKey(slider.name))
+        {
+            t = PlayerPrefs.GetFloat(slider.name);
+            Mixer.audioMixer.SetFloat(slider.name, t);
+        }
+        else Mixer.audioMixer.GetFloat(slider.name, out t);
+        t = Mathf.InverseLerp(-80f, 0f, t);
+        slider.value = t;
+    }
+    public void ChangeVolume(float volume)
+    {
+        float t = Mathf.Lerp(-40, 0, volume);
+        if (t == -40) t = -80;
+        Mixer.audioMixer.SetFloat(mixerGroup, t);
+        PlayerPrefs.SetFloat(mixerGroup, t);
+    }
+    public void SetPitch(float pitch = 0)
+    {
+        source.pitch = pitch;
+    }
+    public void RandomPitch()
+    {
+        source.pitch = Random.Range(0.9f, 2f);
+    }
+}
