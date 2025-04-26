1bf2d5c2b5cd655f16bae8b9e4364fa5af10843d
Serugei <serugei.alt@mail.ru>
2025-04-26 23:32:33 +0300

    anim first walk

---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour
+{
+    [SerializeField] float smoothTime;
+    public enum RoadTypes
+    {
+        Guitar,
+        Bass,
+        Drums
+    }
+    [SerializeField] AudioSource[] roads;
+    public void ToggleRoad(RoadTypes type, bool enabled)
+    {
+        float target = enabled ? 1f : 0f;
+
+        if (enabled) roads[(int)type].volume = target;
+        else StartCoroutine(smoothChangeVolume(roads[(int)type], target, smoothTime));
+    }
+
+    IEnumerator smoothChangeVolume(AudioSource source, float target, float time)
+    {
+        float startVolume = source.volume;
+        float t = 0;
+        while (t < time)
+        {
+            t += Time.deltaTime;
+            source.volume = Mathf.InverseLerp(startVolume, target, t);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
