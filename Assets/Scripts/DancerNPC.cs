00fe3d13542b37f477e529c4cab7dcc8fe899a21
Ася <cdcttwc@gmail.com>
2025-04-27 15:04:01 +0300

    moving dancers

--- Assets/Scripts/DancerNPC.cs
+++ Assets/Scripts/DancerNPC.cs
@@ -2,53 +2,93 @@ using System;
 using UnityEngine;
 using System.Collections;
 using Unity.Netcode;
+using Random = UnityEngine.Random;
 
 public class DancerNPC : NetworkBehaviour
 {
+    public int moveSpeed = 5;
+    public SpriteRenderer conversionVal;
     [SerializeField] float HoldDelay = 3f;
     [SerializeField] SpriteRenderer mask;
     [Range(0f, 1f)] float progress;
     Coroutine coroutineTimer;
+
     Coroutine coroutineConverter;
     Instrument player;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Color baseColor;
+    private Coroutine movingCoroutine;
+    private PlayerMovement curPlayer;
+    private MusicInstrument curPlayerInstrument;
+
+    private float maxConversionValSize;
+    public float ConversionValue { get; set; }
+
     private void Start()
     {
         QTEManager.OnQTEPassed.AddListener(ctx => TogglePlaying(true));
         QTEManager.OnQTEMissed.AddListener(ctx => TogglePlaying(false));
+        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        maxConversionValSize = conversionVal.transform.localScale.x;
+        baseColor = mask.color;
+
+        ConversionValue = 0;
+
+        conversionVal.size = new Vector2(0, conversionVal.size.y);
+        mask.color = new Color(baseColor.r, baseColor.g, baseColor.b, 0);
+
+        Invoke(nameof(RandomMovement), Random.Range(1f, 6f));
     }
+
+    private void FixedUpdate()
+    {
+        mask.sprite = GetComponent<SpriteRenderer>().sprite;
+        mask.flipX = GetComponent<SpriteRenderer>().flipX;
+
+        animator.SetBool("Walking", Mathf.FloorToInt(rb.linearVelocityX) != 0);
+    }
+
     void TogglePlaying(bool enabled)
     {
         if (player == default || player.OwnerClientId != NetworkManager.LocalClientId) return;
         if (enabled)
         {
             if (coroutineTimer != null) StopCoroutine(coroutineTimer);
-            if (coroutineConverter == default) coroutineConverter = StartCoroutine(converse(player.currentConversionSpeed, player.playerColor));
+            if (coroutineConverter == default) coroutineConverter = StartCoroutine(Converse(player.currentConversionSpeed, player.playerColor));
         }
         else
         {
-            if(coroutineConverter!= null) StopCoroutine(coroutineConverter);
+            if (coroutineConverter != null) StopCoroutine(coroutineConverter);
             if (coroutineTimer == default) coroutineTimer = StartCoroutine(HoldTimer(HoldDelay));
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (player != default) return;
         TryGetComponent(out player);
+        
+        // mask.color = new Color(curPlayerInstrument.playerColor.r, curPlayerInstrument.playerColor.g, curPlayerInstrument.playerColor.b, conversionSprite.color.a);
+        if (IsInvoking(nameof(UndoConverse))) CancelInvoke(nameof(UndoConverse));
+        if (!IsInvoking(nameof(Converse))) InvokeRepeating(nameof(Converse), 1f, 1f);
     }
-    private void OnTriggerExit(Collider other)
-    {
-        TogglePlaying(false);
-    }/*
+/*
     [Rpc(SendTo.NotMe)]
     void ConvertingRpc(float speed, bool )
     {
         coroutineTimer = StartCoroutine(converse());
     }*/
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (coroutineTimer != null) StopCoroutine(coroutineTimer);
     }
-    IEnumerator converse(float currentConversionSpeed, Color color)
+
+    IEnumerator Converse(float currentConversionSpeed, Color color)
     {
         float MaxValue = 100, t = 0;
         while (progress < 1)
@@ -59,6 +99,17 @@ public class DancerNPC : NetworkBehaviour
             yield return new WaitForFixedUpdate();
         }
     }
+    
+    private void UndoConverse()
+    {
+        if (ConversionValue > 0.1f)
+            ConversionValue -= 0.1f;
+
+        // conversionSprite.color = Color.Lerp(conversionSprite.color, baseColor, ConversionValue / 10);
+        conversionVal.color = Color.Lerp(conversionVal.color, baseColor, ConversionValue / 10);
+        conversionVal.size = new Vector2(ConversionValue / maxConversionValSize, conversionVal.size.y);
+    }
+
     IEnumerator HoldTimer(float Delay)
     {
         yield return new WaitForSeconds(Delay);
@@ -66,50 +117,30 @@ public class DancerNPC : NetworkBehaviour
         mask.color = Color.black;
     }
 
-/*
-
-    public int moveSpeed = 5;
-    public SpriteRenderer conversionVal, conversionSprite;
-
-    private Rigidbody2D rb;
-    private Color baseColor;
-    private Coroutine movingCoroutine;
-    private PlayerMovement curPlayer;
-    private MusicInstrument curPlayerInstrument;
-    private Animator animator;
-
-    public float ConversionValue { get; set; }
-
-    private void Start()
+    private void RandomMovement()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        baseColor = conversionSprite.color;
+        float rand = Random.Range(-1, 1);
+        GetComponent<SpriteRenderer>().flipX = rand < 0;
+        rb.linearVelocity = new Vector2(rand < 0 ? -moveSpeed : moveSpeed, rb.linearVelocity.y);
 
-        ConversionValue = 0;
+        Invoke(nameof(MovementPause), Random.Range(2f, 11f));
+    }
 
-        conversionVal.size = new Vector2(0, conversionVal.size.y);
-        conversionSprite.color = new Color(baseColor.r, baseColor.g, baseColor.b, 0);
-
+    private void MovementPause()
+    {
+        rb.linearVelocity = Vector2.zero;
         Invoke(nameof(RandomMovement), Random.Range(1f, 6f));
     }
+}
 
-    private void FixedUpdate()
-    {
-        conversionSprite.sprite = GetComponent<SpriteRenderer>().sprite;
-        conversionSprite.flipX = GetComponent<SpriteRenderer>().flipX;
 
-        animator.SetBool("Walking", Mathf.FloorToInt(rb.linearVelocityX) != 0);
-    }
+/*
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.TryGetComponent(out curPlayerInstrument)) return;
 
         curPlayer = other.GetComponent<PlayerMovement>();
-        conversionSprite.color = new Color(curPlayerInstrument.playerColor.r, curPlayerInstrument.playerColor.g, curPlayerInstrument.playerColor.b, conversionSprite.color.a);
-        if (IsInvoking(nameof(UndoConverse))) CancelInvoke(nameof(UndoConverse));
-        if (!IsInvoking(nameof(Converse))) InvokeRepeating(nameof(Converse), 1f, 1f);
     }
 
     private void OnTriggerStay2D(Collider2D other)
@@ -152,28 +183,4 @@ public class DancerNPC : NetworkBehaviour
         conversionVal.size = new Vector2(ConversionValue / maxConversionValSize, conversionVal.size.y);
     }
 
-    private void UndoConverse()
-    {
-        if (ConversionValue > 0.1f)
-            ConversionValue -= 0.1f;
-
-        conversionSprite.color = Color.Lerp(conversionSprite.color, baseColor, ConversionValue / 10);
-        conversionVal.color = Color.Lerp(conversionVal.color, baseColor, ConversionValue / 10);
-        conversionVal.size = new Vector2(ConversionValue / maxConversionValSize, conversionVal.size.y);
-    }
-
-    private void RandomMovement()
-    {
-        float rand = Random.Range(-1, 1);
-        GetComponent<SpriteRenderer>().flipX = rand < 0;
-        rb.linearVelocity = new Vector2(rand < 0 ? -moveSpeed : moveSpeed, rb.linearVelocity.y);
-
-        Invoke(nameof(MovementPause), Random.Range(2f, 11f));
-    }
-
-    private void MovementPause()
-    {
-        rb.linearVelocity = Vector2.zero;
-        Invoke(nameof(RandomMovement), Random.Range(1f, 6f));
-    }*/
-}
+    */
